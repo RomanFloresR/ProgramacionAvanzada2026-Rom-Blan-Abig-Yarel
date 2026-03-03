df1af7a3516fa2585c26cca20ba30a8ed015ff91
blanca502 <zs24013212@estudiantes.uv.mx>
2026-03-03 16:50:24 -0600

    actualización a la visión 1.0.1
    arregle la formula y una descripción

---
+++ Practica1/Python/Versión 1.0.1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 27 08:47:51 2026
+
+@author: abiga
+
+programa que se encarga de sacarle el area a un triangulo 
+"""
+
+#Versión 1.0.0 , en esta versión se encuentra un error.
+#Version 1.0.1 correccin de formula, descripcion agregada - Blanca
+
+
+base=10
+altura=5
+area=(base*altura)/2
+print("El area es igual a ", area)
