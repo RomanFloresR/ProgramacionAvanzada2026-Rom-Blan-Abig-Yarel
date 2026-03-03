3cf1600cf46bb033642899b3ac2ae476927798a7
theeeYareeee <zs24013207@estudiantes.uv.mx>
2026-03-02 18:53:39 -0600

    agregue primera versión del programa
    primera versión del programa que permite hacer el cálculo del área de un triangulo

---
+++ Practica1/Triangulo1.0.0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// 1.0.0 – 2026-03-02 – Yareli del Carmen
+// Programa que encuentra el área de un triangulo  
+
+int main() {
+   
+    float base = 10;
+    float altura = 5;
+    float area;
+
+    area = (base * altura);
+    printf("El resultado es: %.2f\n", area);
+
+    return 0;
+}
