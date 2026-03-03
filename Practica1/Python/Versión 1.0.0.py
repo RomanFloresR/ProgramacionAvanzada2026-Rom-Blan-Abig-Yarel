945c5cf0c339f0671e183deefb59e3d7c1c7d826
abigaillets <zs24013140@estudiantes.uv.mx>
2026-03-02 18:58:56 -0600

    version 1.0.0

---
+++ Practica1/Python/Versión 1.0.0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 27 08:47:51 2026
+
+@author: abiga
+"""
+
+#Versión 1.0.0 , en esta versión se encuentra un error.
+base=10
+altura=5
+area=base*altura
+print("El area es igual a ", area)
