a4f02d63a207a1e769d93563cb272a6992edc27a
theeeYareeee <zs24013207@estudiantes.uv.mx>
2026-03-03 17:07:55 -0600

    actualización
    se le agrego para pedirle datos al usuario

---
+++ Practica1/Python/Versión 1.1.0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 27 08:47:51 2026
+
+@author: abiga
+
+programa que se encarga de sacarle el area a un triangulo 
+"""
+
+#Versión 1.0.0 , en esta versión se encuentra un error.
+#Version 1.0.1 correccin de formula, descripcion agregada - Blanca
+#Version 1.1.0 pedir datos al usuario para el calculo - Yareli  
+
+base= float(input("ingresa la base: "))
+altura= float (input ("ingresa la altura: "))
+area=(base*altura)/2
+print("El area es igual a ", area)
